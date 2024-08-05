96d00b8f685c36e3f26b46c65a1ebc1d9f0e030e
TK <tkosudi@gmail.com>
2024-08-05 12:50:45 -0300

    chore: update eslint and jest to use with axios on tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,31 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import eslintPluginJest from "eslint-plugin-jest";
+import eslintConfigStandard from "eslint-config-standard";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      jest: eslintPluginJest,
+    },
+    rules: {
+      ...eslintConfigStandard.rules,
+    },
+    env: {
+      "jest/globals": true,
+      browser: true,
+      node: true,
+      es6: true,
+    },
+  },
   pluginJs.configs.recommended,
 ];
