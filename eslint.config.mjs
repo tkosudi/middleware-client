25d4a1aae0964f655bd97564bcbd5b50c3c16dbb
TK <tkosudi@gmail.com>
2024-08-06 11:02:59 -0300

    refactor(client): update ApiClient to throw correct answers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslintPluginJest from "eslint-plugin-jest";
-import eslintConfigStandard from "eslint-config-standard";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintPluginJest from 'eslint-plugin-jest';
+import eslintConfigStandard from 'eslint-config-standard';
 
 export default [
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     plugins: {
       jest: eslintPluginJest,
     },
     rules: {
       quotes: ['error', 'single'],
+      'no-unused-vars': ['error', { args: 'none' }],
       ...eslintConfigStandard.rules,
     },
     env: {
-      "jest/globals": true,
+      'jest/globals': true,
       browser: true,
       node: true,
       es6: true,
--- src/client/api-client.js
+++ src/client/api-client.js
@@ -1,20 +1,17 @@
-import axios from "axios"
-import { AbstractClient } from "../core/abstract-client"
-import { ApiError } from "../errors/api-error"
-import { UnauthorizedError } from "../errors/unauthorized-error"
-import { NoResponseError } from "../errors/no-response-error"
-import { RequestError } from "../errors/request-error"
-
+import axios from 'axios'
+import { AbstractClient } from '../core/abstract-client.js'
+import { ApiError } from '../errors/api-error.js'
+import { UnauthorizedError } from '../errors/unauthorized-error.js'
+import { NoResponseError } from '../errors/no-response-error.js'
+import { RequestError } from '../errors/request-error.js'
 
 export class ApiClient extends AbstractClient {
   /**
    * Intermediação para chamadas à API
-   * 
    * @param {string} endpoint
    * @param {string} method
-   * @param {string} data
+   * @param {Object} data
    */
-
   async call(endpoint, method, data) {
     try {
       const response = await axios({
@@ -29,19 +26,19 @@ export class ApiClient extends AbstractClient {
 
       return response.data
     } catch (error) {
-
       if (error.response) {
-        if (error.status === 401) {
-          throw UnauthorizedError(error.response.message, error.response.status)
+        if (error.response.status === 401) {
+          throw new UnauthorizedError(error.response.data.error)
         }
-        throw ApiError(error.response.message, error.response.status)
+
+        throw new ApiError(error.response.data.error, error.response.status)
       }
 
       if (error.request) {
-        throw NoResponseError()
+        throw new NoResponseError()
       }
 
-      throw new RequestError()
+      throw new RequestError(error.message)
     }
   }
 }
