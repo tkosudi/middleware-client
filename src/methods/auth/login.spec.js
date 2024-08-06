b66ffb37919ab4cd2a37e374100edc7242102e4b
TK <tkosudi@gmail.com>
2024-08-06 18:27:43 -0300

    test(login): ensure login fails with incorrect values

--- src/methods/auth/login.spec.js
+++ src/methods/auth/login.spec.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { ApiClient } from "../client/api-client"
 import { Login } from "./login"
+import { UnauthorizedError } from "../../errors/unauthorized-error"
 
 jest.mock('axios')
 
@@ -15,14 +16,24 @@ describe('Login', () => {
 
 
   test('Should successfully login', async () => {
-    const credentials = { email: 'any_email', password: 'any_password' };
-    const token = 'fake-jwt-token';
+    const credentials = { email: 'any_email', password: 'any_password' }
+    const token = 'fake-jwt-token'
 
     axios.mockResolvedValue({
       data: { token }
-    });
+    })
 
-    const response = await login.execute(credentials);
-    expect(response.token).toBe(token);
+    const response = await login.execute(credentials)
+    expect(response.token).toBe(token)
+  })
+
+  test('should throw authentication error for invalid credentials', async () => {
+    const credentials = { email: 'any_email', password: 'wrong-password' }
+
+    axios.mockRejectedValue({
+      response: { status: 401 }
+    })
+
+    await expect(login.execute(credentials)).rejects.toThrow(UnauthorizedError)
   })
 })
