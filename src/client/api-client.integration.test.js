e04c63df235eaf0ab8063ddf34a153742a7c975f
TK <tkosudi@gmail.com>
2024-08-06 15:43:29 -0300

    test(client): ensure ApiClient returns token on successful login

---
+++ src/client/api-client.integration.test.js
@@ -0,0 +1,21 @@
+import { ApiClient } from "./api-client";
+
+const credentials = { token: '' };
+const client = new ApiClient(credentials);
+
+describe('ApiClient Integration Test - Login', () => {
+  const endpoint = 'https://357vtrxigb.execute-api.us-east-1.amazonaws.com/prd/v1/auth/login';
+  const method = 'POST';
+  const data = {
+    email: 'livecode@integra.do',
+    password: 'issoehumteste'
+  }
+
+
+  test('Should login successfully and receive a token', async () => {
+    const result = await client.call(endpoint, method, data)
+    expect(result).toHaveProperty('token')
+    expect(result).toHaveProperty('version')
+    credentials.token = result.token
+  });
+});
