57bdd21d7c205ff934d9e1964cdae90b0df41cb2
TK <tkosudi@gmail.com>
2024-08-07 17:22:08 -0300

    test(product): ensure SendProduct throws if required field are missing

--- src/methods/product/send-product.js
+++ src/methods/product/send-product.js
@@ -1,19 +1,18 @@
-import { AbstractMethod } from '../../core/abstract-method'
-import { RequestError } from '../../errors/request-error'
+import { AbstractMethod } from '../../core/abstract-method.js'
+import { ApiError } from '../../errors/api-error.js'
+import { RequestError } from '../../errors/request-error.js'
 
 export class SendProduct extends AbstractMethod {
   /**
-   * Encaminha produtos para a API
+   * Encaminha um produto para a API
    * @param {Object} product
    * @returns {Object}
    */
-
   async execute(product) {
     const endpoint = 'https://357vtrxigb.execute-api.us-east-1.amazonaws.com/prd/v1/product'
     const method = 'POST'
 
     const requiredFields = ['sku', 'name', 'description']
-
     for (const field of requiredFields) {
       if (!product[field]) {
         throw new RequestError(`Missing required field: ${field}`)
--- src/methods/product/send-product.spec.js
+++ src/methods/product/send-product.spec.js
@@ -1,6 +1,7 @@
 import { SendProduct } from "../product/send-product"
 import { ApiClient } from "../client/api-client"
 import axios from "axios"
+import { RequestError } from "../../errors/request-error"
 
 jest.mock('axios')
 
@@ -63,6 +64,43 @@ describe('SendProduct', () => {
       'POST',
       productData
     )
+  })
+
+  test('Should throw error if required fields are missing', async () => {
+    const productData = {
+      client_id: '66a157abe244d77781ba32cc',
+      reference_id: 158641,
+      ean: 484546544489,
+      weight: 0.1,
+      status: 'active',
+      short_description: 'Produto teste com finalidade de definição de schema.',
+      height: 0.2,
+      width: 0.3,
+      length: 0.1,
+      additional_attributes: [
+        {
+          key: 'inmetro_code',
+          label: 'Código do Inmetro',
+          value: '6750/2014'
+        },
+        {
+          key: 'color',
+          label: 'Cor',
+          value: 'preto'
+        }
+      ],
+      image_list: [
+        {
+          url: 'https://www.teste.com.br/pasta/iphone.jpg',
+          position: 1,
+          label: 'Teste',
+          main_image: true
+        }
+      ]
+    };
+
+    await expect(sendProduct.execute(productData)).rejects.toThrow(RequestError);
+    await expect(sendProduct.execute(productData)).rejects.toThrow('Missing required field: sku');
   })
 
 })
