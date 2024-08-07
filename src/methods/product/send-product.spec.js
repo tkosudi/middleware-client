288ba103427b4bbac0f8513a2397da5fe2f9bd40
TK <tkosudi@gmail.com>
2024-08-07 17:31:47 -0300

    refactor(test): move product to a factory method

--- src/methods/product/send-product.spec.js
+++ src/methods/product/send-product.spec.js
@@ -5,6 +5,41 @@ import { RequestError } from "../../errors/request-error"
 
 jest.mock('axios')
 
+const makeFakeProduct = () => ({
+  client_id: '66a157abe244d77781ba32cc',
+  sku: 'd701748f0999',
+  name: 'Produto teste',
+  reference_id: 158641,
+  ean: 484546544489,
+  weight: 0.1,
+  status: 'active',
+  description: 'Produto teste com finalidade de definição de schema.',
+  short_description: 'Produto teste com finalidade de definição de schema.',
+  height: 0.2,
+  width: 0.3,
+  length: 0.1,
+  additional_attributes: [
+    {
+      key: 'inmetro_code',
+      label: 'Código do Inmetro',
+      value: '6750/2014'
+    },
+    {
+      key: 'color',
+      label: 'Cor',
+      value: 'preto'
+    }
+  ],
+  image_list: [
+    {
+      url: 'https://www.teste.com.br/pasta/iphone.jpg',
+      position: 1,
+      label: 'Teste',
+      main_image: true
+    }
+  ]
+})
+
 describe('SendProduct', () => {
 
   let client
@@ -21,43 +56,8 @@ describe('SendProduct', () => {
   })
 
   test('Should call SendProduct with correct values', async () => {
-    const productData = {
-      client_id: '66a157abe244d77781ba32cc',
-      sku: 'd701748f0999',
-      name: 'Produto teste',
-      reference_id: 158641,
-      ean: 484546544489,
-      weight: 0.1,
-      status: 'active',
-      description: 'Produto teste com finalidade de definição de schema.',
-      short_description: 'Produto teste com finalidade de definição de schema.',
-      height: 0.2,
-      width: 0.3,
-      length: 0.1,
-      additional_attributes: [
-        {
-          key: 'inmetro_code',
-          label: 'Código do Inmetro',
-          value: '6750/2014'
-        },
-        {
-          key: 'color',
-          label: 'Cor',
-          value: 'preto'
-        }
-      ],
-      image_list: [
-        {
-          url: 'https://www.teste.com.br/pasta/iphone.jpg',
-          position: 1,
-          label: 'Teste',
-          main_image: true
-        }
-      ]
-    }
-
+    const productData = makeFakeProduct()
     const response = await sendProduct.execute(productData)
-
     expect(response).toEqual({ success: true })
     expect(client.call).toHaveBeenCalledWith(
       'https://357vtrxigb.execute-api.us-east-1.amazonaws.com/prd/v1/product',
@@ -67,40 +67,9 @@ describe('SendProduct', () => {
   })
 
   test('Should throw error if required fields are missing', async () => {
-    const productData = {
-      client_id: '66a157abe244d77781ba32cc',
-      reference_id: 158641,
-      ean: 484546544489,
-      weight: 0.1,
-      status: 'active',
-      short_description: 'Produto teste com finalidade de definição de schema.',
-      height: 0.2,
-      width: 0.3,
-      length: 0.1,
-      additional_attributes: [
-        {
-          key: 'inmetro_code',
-          label: 'Código do Inmetro',
-          value: '6750/2014'
-        },
-        {
-          key: 'color',
-          label: 'Cor',
-          value: 'preto'
-        }
-      ],
-      image_list: [
-        {
-          url: 'https://www.teste.com.br/pasta/iphone.jpg',
-          position: 1,
-          label: 'Teste',
-          main_image: true
-        }
-      ]
-    };
-
+    const productData = makeFakeProduct()
+    delete productData.sku
     await expect(sendProduct.execute(productData)).rejects.toThrow(RequestError);
     await expect(sendProduct.execute(productData)).rejects.toThrow('Missing required field: sku');
   })
-
 })
